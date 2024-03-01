5a8384c1dac995324e15631a1475a5430da6b203
Nour Meriem Bouayed <bouayed.nour@gmail.com>
2024-03-01 07:46:08 +0100

    correction

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 from evaluate import evaluate_HIV, evaluate_HIV_population
-from train_FQI import ProjectAgent  # Replace DummyAgent with your agent implementation
+from train_DQN import ProjectAgent  # Replace DummyAgent with your agent implementation
 
 
 def seed_everything(seed: int = 42):
@@ -21,7 +21,7 @@ def seed_everything(seed: int = 42):
 if __name__ == "__main__":
     seed_everything(seed=42)
     # Initialization of the agent. Replace DummyAgent with your custom agent implementation.
-    agent = ProjectAgent('FQI')
+    agent = ProjectAgent()
     agent.load()
     # Keep the following lines to evaluate your agent unchanged.
     score_agent: float = evaluate_HIV(agent=agent, nb_episode=1)
