2dbdc10cbbb38ea0287c0648ac2bc579e9ba2120
Nour Meriem Bouayed <bouayed.nour@gmail.com>
2024-03-02 16:34:31 +0100

    7

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def seed_everything(seed: int = 42):
 if __name__ == "__main__":
     seed_everything(seed=42)
     # Initialization of the agent. Replace DummyAgent with your custom agent implementation.
-    agent = ProjectAgent('DQN_6')
+    agent = ProjectAgent('DQN_7_dr')
     agent.load()
     # Keep the following lines to evaluate your agent unchanged.
     score_agent: float = evaluate_HIV(agent=agent, nb_episode=1)
